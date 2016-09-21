b1b6cd68a4d2072515e69a0432d8886b937c1b9d
Mary Ehlers <regina.verb.ae@gmail.com>
2016-09-21 19:18:05 -0400

    Documentation for Piper::Config

--- lib/Piper/Config.pm
+++ lib/Piper/Config.pm
@@ -1,6 +1,6 @@
 #####################################################################
 ## AUTHOR: Mary Ehlers, regina.verbae@gmail.com
-## ABSTRACT: Component configuration object for Piper
+## ABSTRACT: Configuration object for Piper
 #####################################################################
 
 package Piper::Config;
@@ -15,25 +15,52 @@ use Types::Standard qw(ClassName);
 use Moo;
 use namespace::clean;
 
-=head1 CONSTRUCTOR
+=head1 SYNOPSIS
 
-=head2 new
+  # Defaults
+  use Piper (
+      batch_size   => 200,
+      logger_class => 'Piper::Logger',
+      queue_class  => 'Piper::Queue',
+  );
 
-=cut
+=head1 DESCRIPTION
+
+A configuration object is instantiated during import
+of the Piper module according to any supplied import
+arguments.
 
 =head1 ATTRIBUTES
 
 =head2 batch_size
+
+The default batch size used by pipeline segments
+which do not have a locally defined batch_size and
+do not have a parent segment with a defined
+batch_size.
+
+The batch_size attribute must be a positive integer.
+
+The default batch_size is 200.
 
 =cut
 
 has batch_size => (
     is => 'lazy',
     isa => PositiveInt,
-    default => 50,
+    default => 200,
 );
 
 =head2 logger_class
+
+The logger_class is used for printing debug and
+info statements, issuing warnings, and throwing
+errors.
+
+The logger_class attribute must be a valid class
+that does the role defined by Piper::Role::Logger.
+
+The default logger_class is Piper::Logger.
 
 =cut
 
@@ -52,6 +79,14 @@ has logger_class => (
 );
 
 =head2 queue_class
+
+The queue_class handles the queueing of data
+for each of the pipeline segments.
+
+The queue_class attribute must be a valid class
+that does the role defined by Piper::Role::Queue.
+
+The default queue_class is Piper::Queue.
 
 =cut
 
