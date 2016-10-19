060371fdaa3f9bc6a821b3b99d494c7386d5fd79
Mary Ehlers <regina.verb.ae@gmail.com>
2016-10-19 06:59:24 -0400

    Documentation++

--- lib/Piper/Role/Segment.pm
+++ lib/Piper/Role/Segment.pm
@@ -17,18 +17,13 @@ use Moo::Role;
 
 =head1 DESCRIPTION
 
-This role contains attributes and methods that apply
-to each pipeline segment, both individual process
-handlers and pipelines.
+This role contains attributes and methods that apply to each pipeline segment, both individual process handlers (L<Piper::Process>) and pipelines (L<Piper>).
 
 =head1 REQUIRES
 
 =head2 init
 
-This role requires the definition of an init method
-which initializes the segment as a pipeline
-instance and prepares it for data processing.  The
-method must return the created pipeline instance.
+This role requires the definition of an C<init> method which initializes the segment as a pipeline instance and prepares it for data processing.  The method must return the created pipeline instance.
 
 =cut
 
@@ -61,19 +56,13 @@ around init => sub {
 
 =head2 allow
 
-An optional coderef used to subset the items which
-are allowed to be processed by the segment.
+An optional coderef used to subset the items which are I<allowed> to be processed by the segment.
 
-The coderef runs on each item attempting to queue
-to the segment.  If it returns true, the item is
-queued.  Otherwise, the item skips the segment and
-proceeds to the next adjacent segment.
+The coderef runs on each item attempting to queue to the segment.  If it returns true, the item is queued.  Otherwise, the item skips the segment and proceeds to the next adjacent segment.
 
-Each item is localized to $_, and is also passed in
-as the first argument.  These example 'allow'
-subroutines are equivalent:
+Each item is localized to C<$_>, and is also passed in as the first argument.  These example C<allow> subroutines are equivalent:
 
-    # This handler only accepts digit inputs
+    # This segment only accepts digit inputs
     sub { /^\d+$/ }
     sub { $_ =~ /^\d+$/ }
     sub { $_[0] =~ /^\d+$/ }
@@ -99,9 +88,7 @@ has allow => (
 
 =head2 batch_size
 
-The number of items to process at a time for
-the segment.  A segment inherits the batch_size
-of its parent(s) if not provided.
+The number of items to process at a time for the segment.  Once initialized, a segment inherits the C<batch_size> of its parent(s) if not provided.
 
 =cut
 
@@ -115,7 +102,9 @@ has batch_size => (
 
 =head2 config
 
-A Piper::Config object defining component classes.
+A L<Piper::Config> object defining component classes and global defaults.
+
+This attribute is set according to the import options provided to S<C<use Piper>>.
 
 =cut
 
@@ -141,8 +130,7 @@ has debug => (
 
 =head2 enabled
 
-Boolean indicating that the segment is enabled and
-can accept items for processing.  Defaults to true.
+Boolean indicating that the segment is enabled and can accept items for processing.  Defaults to true.
 
 =cut
 
@@ -156,8 +144,7 @@ has enabled => (
 
 =head2 id
 
-A globally uniq ID for the segment.  This is primarily
-useful for debugging only.
+A globally uniq ID for the segment.  This is primarily useful for debugging only.
 
 =cut
 
@@ -175,12 +162,9 @@ has id => (
 
 =head2 label
 
-A label for this segment.  If no label is provided, the
-segment's id will be used.
+A label for this segment.  If no label is provided, the segment's id will be used.
 
-Labels are necessary if any handlers wish to use the
-injectAt or injectAfter methods.  Otherwise, labels are
-primarily useful for logging and/or debugging.
+Labels are necessary if any handlers wish to use the C<injectAt> or C<injectAfter> methods (described in L<Piper> or L<Piper::Process> documentation).  Otherwise, labels are primarily useful for logging and/or debugging (see L<Piper::Logger>).
 
 =cut
 
@@ -212,42 +196,60 @@ has verbose => (
 
 =head2 clear_batch_size
 
-Clears any assigned batch_size for the segment.
+Clears any assigned C<batch_size> for the segment.
 
 =head2 clear_debug
 
-Clears any assigned debug level for the segment.
+Clears any assigned C<debug> level for the segment.
 
 =head2 clear_enabled
 
-Clears any assigned enabled setting for the segment.
+Clears any assigned C<enabled> setting for the segment.
 
 =head2 clear_verbose
 
-Clears any assigned verbosity level for the segment.
+Clears any assigned C<verbose> level for the segment.
 
 =head2 has_allow
 
-A boolean indicating whether or not an 'allow'
-attribute exists for this segment.
+A boolean indicating whether or not an C<allow> attribute exists for this segment.
 
 =head2 has_batch_size
 
-A boolean indicating whether the segment
-has an assigned batch_size.
+A boolean indicating whether the segment has an assigned C<batch_size>.
 
 =head2 has_debug
 
-A boolean indicating whether the segment has an assigned debug level.
+A boolean indicating whether the segment has an assigned C<debug> level.
 
 =head2 has_enabled
 
-A boolean indicating whether the segment has an assigned enabled setting.
+A boolean indicating whether the segment has an assigned C<enabled> setting.
 
 =head2 has_verbose
 
-A boolean indicating whether the segment has an assigned verbosity level.
+A boolean indicating whether the segment has an assigned C<verbose> level.
 
 =cut
 
 1;
+
+__END__
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Piper>
+
+=item L<Piper::Process>
+
+=item L<Piper::Instance>
+
+=item L<Piper::Logger>
+
+=item L<Piper::Config>
+
+=back
+
+=cut
